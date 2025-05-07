a159eb4746f3d78999c5f5864f373322bb6791a2
Tia Hedlund <tia.hedlund@elev.ga.ntig.se>
2025-05-07 12:35:06 +0200

    Skrivit in koden och anget protnumber

--- src/MulticastChatServer.java
+++ src/MulticastChatServer.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
+import java.net.*;
 public class MulticastChatServer {
+
+public static void main(String args[]) throws Exception {
+        int portnumber = 53004;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+
+        boolean infinite = true;
+
+        while (infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            System.out.println("Message received from client = " + msg);
+        }
+
+        serverMulticastSocket.close();
+    }
 }
+
